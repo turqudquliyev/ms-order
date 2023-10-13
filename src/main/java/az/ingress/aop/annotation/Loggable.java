e3df060a0918cd59b442bed4a43d01160800fe58
--replace-all <turqudquliyev@gmail.com>
2023-10-13 23:48:01 +0400

    Added gradle files

--- src/main/java/az/ingress/aop/annotation/Loggable.java
+++
@@ -1,8 +0,0 @@
-package az.ingress.aop.annotation;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-@Target(METHOD)
-@Retention(RUNTIME)
-public @interface Loggable { }
--- src/main/java/az/ingress/queue/handler/PaymentQueueConsumer.java
+++ src/main/java/az/ingress/queue/handler/PaymentQueueConsumer.java
@@ -4,11 +4,13 @@
 //import az.ingress.model.queue.PaymentRequest;
 //import az.ingress.model.response.OrderResponse;
 //import az.ingress.service.OrderService;
+//import com.fasterxml.jackson.core.JsonProcessingException;
 //import lombok.RequiredArgsConstructor;
 //import lombok.experimental.FieldDefaults;
 //import org.springframework.amqp.rabbit.annotation.RabbitListener;
 //import org.springframework.stereotype.Service;
 //
+//import static az.ingress.mapper.ObjectMapperFactory.OBJECT_MAPPER;
 //import static az.ingress.mapper.OrderMapper.ORDER_MAPPER;
 //import static az.ingress.model.enums.OrderStatus.PLACED;
 //import static lombok.AccessLevel.PRIVATE;
@@ -20,10 +22,15 @@
 //    OrderService orderService;
 //
 //    @RabbitListener(queues = "${rabbitmq.queue.payment}")
-//    public void placeOrder(PaymentRequest request) {
-//        OrderResponse orderResponse = orderService.getOrderById(request.getOrderId());
-//        orderResponse.setStatus(PLACED);
-//        OrderEntity order = ORDER_MAPPER.mapOrderResponseToOrderEntity(orderResponse);
-//        orderService.save(order);
+//    public void placeOrder(String message) {
+//        try {
+//            PaymentRequest paymentRequest = OBJECT_MAPPER.createInstance().readValue(message, PaymentRequest.class);
+//            OrderResponse orderResponse = orderService.getOrderById(paymentRequest.getOrderId());
+//            orderResponse.setStatus(PLACED);
+//            OrderEntity order = ORDER_MAPPER.mapOrderResponseToOrderEntity(orderResponse);
+//            orderService.save(order);
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
 //    }
 //}
