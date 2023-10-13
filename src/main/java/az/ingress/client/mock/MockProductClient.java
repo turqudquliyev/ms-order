a22ac4c48f3257d9b0f6d3884f3ce42e73afb7c6
--replace-all <turqudquliyev@gmail.com>
2023-10-13 23:53:25 +0400

    Profiling added

--- src/main/java/az/ingress/client/mock/MockProductClient.java
+++ src/main/java/az/ingress/client/mock/MockProductClient.java
@@ -4,11 +4,17 @@ import az.ingress.client.ProductClient;
 import az.ingress.model.client.ProductRequest;
 import az.ingress.model.client.ProductResponse;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import static java.math.BigDecimal.TEN;
 
 @Profile("local")
+@Component
 public class MockProductClient implements ProductClient {
     @Override
     public ProductResponse calculateTotalAmount(ProductRequest request) {
-        return null;
+        return ProductResponse.builder()
+                .totalAmount(TEN)
+                .build();
     }
 }
