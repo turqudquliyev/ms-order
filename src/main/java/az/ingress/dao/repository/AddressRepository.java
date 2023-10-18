4d9657af8daf563dfc7c93c51fa24ca2fbb6be70
--replace-all <turqudquliyev@gmail.com>
2023-10-18 19:05:22 +0400

    JpaRepository to CrudRepository

--- src/main/java/az/ingress/dao/repository/AddressRepository.java
+++ src/main/java/az/ingress/dao/repository/AddressRepository.java
@@ -1,7 +1,7 @@
 package az.ingress.dao.repository;
 
 import az.ingress.dao.entity.AddressEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
+public interface AddressRepository extends CrudRepository<AddressEntity, Long> {
 }
--- src/main/java/az/ingress/dao/repository/OrderRepository.java
+++ src/main/java/az/ingress/dao/repository/OrderRepository.java
@@ -1,10 +1,10 @@
 package az.ingress.dao.repository;
 
 import az.ingress.dao.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
     List<OrderEntity> findByIdIn(List<Long> orderIds);
 }
